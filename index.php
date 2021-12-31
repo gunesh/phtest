0ca3232aa53a792734044b581b179c33f0d4c8fb
gunesh <gunesh26@gmail.com>
2021-12-31 14:54:20 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:atspractic.database.windows.net,1433; Database = Timesheet", "dbamin", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+?>
