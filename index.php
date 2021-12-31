f7229a9150d966b128d1943a6555f4b796cabf9c
gunesh <gunesh26@gmail.com>
2021-12-31 14:56:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:atspractic.database.windows.net,1433; Database = Timesheet", "dbamin", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:atspractic.database.windows.net,1433; Database = Timesheet", "dbamin", "DBadmin@123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
